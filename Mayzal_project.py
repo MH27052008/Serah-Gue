04631f5226af9949da547cbd945444f8632cfd81
MH27052008 <52830432+MH27052008@users.noreply.github.com>
2019-07-15 19:15:43 +0800

    Mayzal_project.py

--- Mayzal_project.py
+++ Mayzal_project.py
@@ -14,7 +14,7 @@ def main():
    pilhan = input('masukan pilihan anda : ')
    if pilhan == '1' or pilhan ==  '01':
       print ('all_seeing')
-      os.system('git clone https://github.com/thelinuxchoice/locator && cd locator && bash locator. sh')
+      os.system('git clone https://github.com/thelinuxchoice/locator && cd locator && bash locator.sh')
    elif pilhan == '2' or pilhan == '02':
         print ('installing trap')
         os.system('git clone https://github.com/zlucifer/trap_project && cd trap_project && bash trap.sh')
