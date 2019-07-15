095208faa34d8ab3c56cf586b7cd5e61a44d4a0f
MH27052008 <52830432+MH27052008@users.noreply.github.com>
2019-07-15 19:09:29 +0800

    Mayzal_project.py

--- Mayzal_project.py
+++ Mayzal_project.py
@@ -12,13 +12,13 @@ menu = """
 def main():
    print (menu)
    pilhan = input('masukan pilihan anda : ')
-   if pilhan == '1' or pilihan ==  '01':
+   if pilhan == '1' or pilhan ==  '01':
       print ('all_seeing')
       os.system('git clone https://github.com/zlucifer/all_seeing && cd all_seeing && bash cctv.sh')
-   elif pilhan == '2' or pilihan == '02':
+   elif pilhan == '2' or pilhan == '02':
         print ('installing trap')
         os.system('git clone https://github.com/zlucifer/trap_project && cd trap_project && bash trap.sh')
-   elif pilhan == '3' or pilihan == '03':
+   elif pilhan == '3' or pilhan == '03':
         print ('installing troll')
         os.system('git clone https://github.com/zlucifer/troll_project && cd troll_project && bash troll.sh')
 
